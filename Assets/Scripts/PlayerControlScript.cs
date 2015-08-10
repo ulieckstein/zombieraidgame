d3536822795415b450326476bbedb98fa06f07f8
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-10 17:29:32 +0200

    Set up project and created player controls

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        rigidbody2D.angularVelocity = 0;
+
+        rigidbody2D.AddForce(gameObject.transform.up * moveSpeed * Input.GetAxis("Vertical"));
+    }
+}
