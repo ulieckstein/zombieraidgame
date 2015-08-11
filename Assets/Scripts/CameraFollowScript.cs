eee4b6d56bb23c3b71105ea49ac07b2c461cdbff
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-11 15:21:29 +0200

    Added Zombie prefab Added Ground Added following Camera Script

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform player;
+
+	// Update is called once per frame
+	void Update () {
+	    transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform player;
+
+    private void FixedUpdate()
+    {
+        float z = Mathf.Atan2((player.transform.position.y - transform.position.y),
+            (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+        transform.eulerAngles = new Vector3(0, 0, z);
+        rigidbody2D.AddForce(gameObject.transform.up * moveSpeed);
+    }
+
+}
