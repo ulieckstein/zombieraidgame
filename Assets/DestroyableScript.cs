91c6515bd85ff0af40ed5155e456f6576829d379
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-12 13:42:33 +0200

    Added Damage Function to the Weapon

---
+++ Assets/DestroyableScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyableScript : MonoBehaviour
+{
+
+    public float Health;
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+        Debug.Log(gameObject.name + " Health: " + Health);
+        if (Health < 0) Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -5,18 +5,53 @@ public class WeaponScript : MonoBehaviour
 {
 
     private Animator _animator;
-    public float HitPoints;
+    
+    public float HitPointsMax;
+    public float HitPointsMin;
+    public int PrecisionPercent;
+    
+    public float PushBackForce;
+    public float PushBackDelay;
 
+    private float _currentDamage;
+    private float _hitAccuracy;
+    
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _currentDamage = 0;
+        _hitAccuracy = 0.0f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _animator.SetTrigger("Attack");
+            Attack();
         }
+    }
+
+    void Attack()
+    {
+        _hitAccuracy = (Random.Range(0, 100) < PrecisionPercent ? 1 : 0);
+        _currentDamage = Random.Range(HitPointsMax, HitPointsMin)*_hitAccuracy;
+        _animator.SetTrigger("Attack");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit something with damage " + _currentDamage);
+        if (other.gameObject.tag == "Enemy")
+        {
+            var destroyable = other.gameObject.GetComponent<DestroyableScript>();
+            destroyable.TakeDamage(_currentDamage);
+            StartCoroutine(PushBack(other.gameObject));
+        }
+    }
+
+    IEnumerator PushBack(GameObject other)
+    {
+        yield return new WaitForSeconds(PushBackDelay);
+        other.rigidbody2D.AddForce(gameObject.transform.up * PushBackForce * _hitAccuracy);
     }
 }
