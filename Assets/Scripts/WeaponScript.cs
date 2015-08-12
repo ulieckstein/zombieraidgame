e43574cbf3214ffd110ef0604ea187de558f4256
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-12 12:28:57 +0200

    Added dummy/default Weapon and Attack animation

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour
+{
+
+    private Animator _animator;
+    public float HitPoints;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetTrigger("Attack");
+        }
+    }
+}
