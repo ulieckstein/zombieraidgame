b0298c092b732e6060e637effbf5e40e1be0085e
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-17 11:11:58 +0200

    Added basic HitAnimation

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -12,11 +12,15 @@ public class WeaponScript : MonoBehaviour
 
     private float _currentDamage;
     private float _hitAccuracy;
-    
+
+    public GameObject HitAnimation;
+    private Animator HitAnimationController;
+
     void Start()
     {
         _currentDamage = 0;
         _hitAccuracy = 0.0f;
+        HitAnimationController = HitAnimation.GetComponent<Animator>();
     }
 
     public void Attack()
@@ -33,7 +37,15 @@ public class WeaponScript : MonoBehaviour
             var destroyable = other.gameObject.GetComponent<DestroyableScript>();
             destroyable.TakeDamage(_currentDamage);
             StartCoroutine(PushBack(other.gameObject));
+            TriggerHitAnimation(other);
         }
+    }
+
+    private void TriggerHitAnimation(Collider2D other)
+    {
+        HitAnimation.transform.position = other.transform.position; 
+        HitAnimationController.SetFloat("Damage", _currentDamage);
+        HitAnimationController.SetTrigger("Hit");
     }
 
     IEnumerator PushBack(GameObject other)
