f30526bd680f69a1b2d0b5db3662a413db126ffa
Uli Eckstein <u.eckstein@brickmakers.de>
2015-08-19 16:47:54 +0200

    implemented alternative (hotline-miami-like) controls

--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -31,19 +31,33 @@ public class PlayerControlScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        ApplyControlInput();
+    }
+
+    private void ApplyControlInput()
+    {
+        ApplyLineOfSight();
         ApplyMovement();
     }
 
     private void ApplyMovement()
+    {
+        var vertical = Input.GetAxis("Vertical");
+        rigidbody2D.AddForce(Vector2.up * MoveSpeed * vertical);
+        
+        var horizontal = Input.GetAxis("Horizontal");
+        rigidbody2D.AddForce(Vector2.right * MoveSpeed * horizontal);
+
+        var walking = horizontal != 0 || vertical != 0;
+        _animator.SetBool("Walking", walking);
+    }
+
+    private void ApplyLineOfSight()
     {
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Quaternion rotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
         transform.rotation = rotation;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
         rigidbody2D.angularVelocity = 0;
-
-        var vertical = Input.GetAxis("Vertical");
-        rigidbody2D.AddForce(gameObject.transform.up * MoveSpeed * vertical);
-        _animator.SetBool("Walking", vertical != 0);
     }
 }
